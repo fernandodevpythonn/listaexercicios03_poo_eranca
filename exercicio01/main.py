2608e2d2c3cce7c5004ae4067f20b09dd2270f8e
fernandodevpythonn <fernandoribeiro270806@gmail.com>
2026-05-15 16:08:46 -0300

    ajustando

--- exercicio01/main.py
+++ exercicio01/main.py
@@ -14,6 +14,7 @@ def menu():
     print("3 - Cadastrar produto")
     print("4 - mostrar usuário")
     print("5 - mostrar produtos")
+    print("6 - Fechar sistema")
 
 def main():
     while True:
@@ -22,6 +23,7 @@ def main():
         match opcao:
             case "1":
                 usu.realizar_login()
+                input("Aperte Enter para voltar")
             case "2":
                 if usu.perfil == "cliente":
                   prod.mostrar_produtos_cliente()
@@ -29,16 +31,19 @@ def main():
                 elif usu.perfil == "cliente vip":
                   prod.mostrar_produtos_cliente_vip()
                   clivip.fazer_compra_cliente_vip()
+                input("Aperte Enter para voltar")
             case "3":
                  if usu.perfil == "administrador" or "adm":
                    prod.adicionar_produto()
                  else:
                     raise ValueError("erro: precisa ser administrador para acessar esta página")
+                 input("Aperte Enter para voltar")
             case "4":
                 if usu.perfil == "administrador" or usu.perfil == "cliente" or usu.perfil == "cliente vip":
                   usu.mostrar_usuario()
                 else:
                     raise ValueError("erro: perfil inválido")
+                input("Aperte Enter para voltar")
             case "5":
                 if prod.nome != "sem nome":
                   if usu.perfil == "cliente":
@@ -49,6 +54,9 @@ def main():
                     prod.mostrar_produtos()
                 else:
                     print("nenhum produto cadastrado")
-
+                input("Aperte Enter para voltar")
+            case "6":
+              print("sistema fechado")
+              break
 if __name__ == "__main__":
     main()
